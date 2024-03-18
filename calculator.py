5b7172ec7b1dc1755d0c606aa3d7c87739313991
kvl00l <g12460766@tdrive.thu.edu.tw>
2024-03-18 12:05:33 +0800

    Multi calculator

--- calculator.py
+++ calculator.py
@@ -5,13 +5,17 @@
 # 1.Input
 x1 = input("Type x1= ") 
 x2 = input("Type x2= ")
-op = input('Enter operator (+,-): ')
+op = input('Enter operator (+,-,*,/): ')
 
 # 2.Process
 if op == '+':
     sum = int(x1)+int(x2)
 elif op == '-':
     sum = int(x1)-int(x2)
+elif op == '*':
+    sum = int(x1)*int(x2)
+elif op == '/':
+    sum = int(x1)/int(x2)
 
 # 3. Output
 print(f"Sum: {sum}")
